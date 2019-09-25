b95556f351af9ef6be8f9acc6ea79ebfd578df5b
Tay <>
2019-09-25 11:23:07 -0400

    I Worked out What happens when my player collides into objects, Only through comments. I still have to code it :(

--- IntermediateGameDevMidterm(1)/Assets/PlayerControls.cs
+++ IntermediateGameDevMidterm(1)/Assets/PlayerControls.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+
+    public GameObject NPC;
+    public bool hasCassette = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +24,45 @@ public class PlayerControls : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter(Collision other)
+    {
+        //If Player Collides into button1 and hasn't collided into a button yet
+        //Play Friends by twin Xl
+        //CassetteTurnsTrue();
+        //Get a cassette of button's color
+        
+        //If Player Collides into button2and hasn't collided into a button yet
+        //Play BleedMagic by iDKHOW
+        //CassetteTurnsTrue();
+        //Get a cassette of button's color
+        
+       
+        //If Player Collides into button3
+        //Play 
+        //CassetteTurnsTrue();
+        //Get a cassette of button's color
+
+
+        //If Player Collides into Npc
+        //Trigger On NPC For Cassette
+    }
+
+   /* public void OnNPC()
+    {
+        if(hascassette == true)
+        {
+        }
+        
+        
+    }*/
+   
+   /* public void CassetteTurnsTrue ()
+    {
+    
+        //Disable All Buttons
+        //hasCassette == true;
+        
+        
+    }*/
 }
