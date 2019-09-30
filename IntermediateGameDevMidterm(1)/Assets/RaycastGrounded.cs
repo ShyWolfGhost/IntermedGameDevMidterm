2f560827cc5944b3deeae2c9c75425d05e556d2a
Tay <>
2019-09-30 11:41:58 -0400

    New scene not for project, raycast lesson

---
+++ IntermediateGameDevMidterm(1)/Assets/RaycastGrounded.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastGrounded : MonoBehaviour
+{
+    private float myRayDist=1.7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("ffffff");
+        //declareRay
+        Ray myRay = new Ray(transform.position, Vector3.down);
+        
+        Debug.DrawRay(myRay.origin,myRay.direction*myRayDist,Color.cyan);
+
+        if (Physics.Raycast(myRay, myRayDist))
+        {
+            Debug.Log("groundhit");
+        }
+        else
+        {
+            Debug.Log("Off Ground");
+        }
+    }
+}
+//fuck
